e010c4fb10232f4c57f2d6b2c03edacc3038bd25
Anurag Patnaik <anurag.patnaik@gmail.com>
2020-08-17 10:46:10 -0700

    Adding calculations for expected completion date and avg probs per day.

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,7 @@ import pickle
 import os.path
 import pandas as pd
 import plotly.express as px
+from datetime import timedelta
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -91,6 +92,7 @@ def main():
         problems_per_day_cache[key] = int_val - prev_day_probs
         prev_day_probs = int_val
 
+    # Progress Trend Line
     completion_vs_solved = pd.DataFrame(solved_progress_cache.items(), columns=['CompletionDate', 'ProblemsSolved'])
     completion_vs_solved['CompletionDate'] = pd.to_datetime(completion_vs_solved['CompletionDate'])
     completion_vs_solved['ProblemsSolved'] = pd.to_numeric(completion_vs_solved['ProblemsSolved'])
@@ -98,12 +100,26 @@ def main():
     fig = px.line(completion_vs_solved, x='CompletionDate', y='ProblemsSolved')
     fig.show()
 
+    # Problems solved per day
     problems_per_day = pd.DataFrame(problems_per_day_cache.items(), columns=['CompletionDate', 'ProblemsPerDay'])
     problems_per_day['CompletionDate'] = pd.to_datetime(problems_per_day['CompletionDate'])
     problems_per_day['ProblemsPerDay'] = pd.to_numeric(problems_per_day['ProblemsPerDay'])
 
     problems_per_day_graph = px.bar(problems_per_day, x='CompletionDate', y='ProblemsPerDay')
     problems_per_day_graph.show()
+
+    # Expected date of completion
+    num_all_problems = 1500
+    total_problems_solved = int(values[-1][2])
+    num_days_passed = len(values) - 1  # Exclude first title row
+    start_date = pd.to_datetime(values[1][1])
+
+    avg_problems_solved_per_day = total_problems_solved / num_days_passed
+    num_days_all_problems = num_all_problems / avg_problems_solved_per_day
+    expected_completion_date_all_problems = start_date + timedelta(days=num_days_all_problems)
+
+    print(f'Average problems per day: {avg_problems_solved_per_day}')
+    print(f'Expected completion date: {expected_completion_date_all_problems}')
 
 
 if __name__ == '__main__':
